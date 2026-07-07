1842600f244381b43a63e958fa30a371776d9efc
ta23farm <blackhatobus@gmail.com>
2026-07-07 18:24:10 +0900

    support includes

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
 module.exports = function(eleventyConfig) {
+  // BootstrapのCSSをコピー
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/bootstrap/dist/css/bootstrap.min.css": "css/bootstrap.min.css"
+  });
+
+  // BootstrapのJSをコピー
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "js/bootstrap.bundle.min.js"
+  });
   return {
     dir: {
       input: "src",
